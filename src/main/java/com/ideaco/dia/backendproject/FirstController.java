984dd39878cd58e1db12afc20c5fbd4e8fcc969d
Stephen Daniel <stephendanielxxx@gmail.com>
2022-10-27 16:57:51 +0700

    day 1

--- src/main/java/com/ideaco/dia/backendproject/FirstController.java
+++ src/main/java/com/ideaco/dia/backendproject/FirstController.java
@@ -11,22 +11,22 @@ public class FirstController {
     private FirstService firstService;
 
     @Autowired
-    public FirstController(FirstService firstService){
+    public FirstController(FirstService firstService) {
         this.firstService = firstService;
     }
 
     @GetMapping("/helloWorld")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return firstService.manipulateString("Welcome");
     }
 
     @GetMapping("/persons")
-    public List<PersonModel> getAllPerson(){
+    public List<PersonModel> getAllPerson() {
         return firstService.getAllPerson();
     }
 
@@ -34,12 +34,35 @@ public class FirstController {
     public String createPerson(@RequestParam("name") String name,
                                @RequestParam("age") int age,
                                @RequestParam("job") String job,
-                               @RequestParam("address") String address){
-        if(firstService.createPerson(name, age, job, address)){
+                               @RequestParam("address") String address) {
+        if (firstService.createPerson(name, age, job, address)) {
             return "Sukses insert person";
-        }else {
+        } else {
             return "Failed insert person";
         }
+
+    }
+
+    @PostMapping("/person/body")
+    public String createPersonWithBody(@RequestBody PersonRequest personRequest) {
+        if (firstService.createPerson(personRequest.getName(),
+                personRequest.getAge(), personRequest.getJob(),
+                personRequest.getAddress())) {
+            return "Sukses insert person";
+        } else {
+            return "Failed insert person";
+        }
+    }
+
+    @PostMapping("/persons/body")
+    public String createPersonsWithBody(@RequestBody List<PersonRequest> personRequests) {
+        for (PersonRequest person : personRequests) {
+            firstService.createPerson(person.getName(),
+                    person.getAge(), person.getJob(),
+                    person.getAddress());
+        }
+
+        return "Sukses insert person";
 
     }
 
